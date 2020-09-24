781eceb32596c8b32601b78f066cf931538ee514
aolthoff2 <70964419+aolthoff2@users.noreply.github.com>
2020-09-24 11:38:09 -0600

    Changes to OOPs Review

--- OOPsReview/ConsoleApp/Program.cs
+++ OOPsReview/ConsoleApp/Program.cs
@@ -10,7 +10,23 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Window myInstance = new Window(); //the system calls 
+            //Create an instance of the Window Class using the default constructor.
+            //The system calls your class constructor, your code NEVER calls the constructor directly.
+            //The "new" will use the indicated constructor.
+            //The "new" actually makes the call to the constructor and returns the instance of the class.
+
+            //Call to the Default constructor.
+            Window myInstance = new Window(); //the system calls you class constructor
+
+            //Results of the constructor
+            Console.WriteLine($"Width {myInstance.Width}; Height {myInstance.Height}; Panes {myInstance.NumberOfPanes}; Manufacturer {myInstance.Manufacturer}");
+
+            myInstance.Width = 1.2m;
+            myInstance.Height = 1.2m;
+            myInstance.NumberOfPanes = 3;
+            myInstance.Manufacturer = "All-Weather Windows";
+
+            Window myGreedyInstance = new Window(1.2m, 1.2m, 3, "All-Weather Windows");
         }
     }
 }
--- OOPsReview/ConsoleApp/Wall.cs
+++ OOPsReview/ConsoleApp/Wall.cs
@@ -8,5 +8,76 @@ namespace ConsoleApp
 {
     class Wall
     {
+
+        //Height, Width > 0.0
+        //Height has a default of 2.5, width is 4.25m
+        //Area and Perimeter
+        //Throw exceptios for invalid data
+        private decimal _Height;
+        private decimal _Width;
+
+    
+
+    public decimal Height
+    {
+        get
+        {
+            return _Height;
+        }
+
+        set
+        {
+            //The M indicates the value is a decimal
+            if (value <= 0.0m)
+            {
+                throw new Exception("Height can not be 0 or less.");
+            }
+            else
+            {
+                _Height = value;
+            }
+        }
+    }
+
+    public decimal Width
+    {
+        get
+        {
+            return _Width;
+        }
+        set
+        {
+            if (value <= 0.0m)
+            {
+                throw new Exception("Width can not be 0 or less than.");
+            }
+            else
+            {
+                _Width = value;
+            }
+        }
+    }
+    public Wall()
+    {
+        Width = 4.25m;
+        Height = 2.5m;
+    }
+
+    public Wall(decimal width, decimal height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public decimal WallArea()
+    {
+        return Width * Height;
+    }
+
+    public decimal WallPerimeter()
+    {
+        return 2 * (Width * Height);
+    }
+}
     }
 }
